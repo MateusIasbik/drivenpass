7679af6b377615c6e420fde93a8cf766d1b10e6e
Mateus Iasbik <mateusmpi@gmail.com>
2024-10-04 11:19:59 -0300

    fix: post credential

--- src/repositories/credential-repository.ts
+++ src/repositories/credential-repository.ts
@@ -1,17 +1,44 @@
 import { CredentialData, UserPayload } from "protocols";
 import prisma from "../database";
+import Cryptr from 'cryptr';
+
+const cryptr = new Cryptr(process.env.CRYPTR_SECRET as string);
 
 async function insertCredential(credentialData: CredentialData, user: UserPayload) {
 
+    const { title, url, username, password } = credentialData;
+
+    const encryptedPassword = cryptr.encrypt(password);
+
+    const result = await prisma.credential.create({
+        data: {
+            title,
+            url,
+            username,
+            password: encryptedPassword,
+            userId: user.id
+        }
+       });
+    
+       return result;
+}
+
+async function verifyTitle(credentialData: CredentialData, user: UserPayload) {
+    
     const result = await prisma.credential.findFirst({
-        where: {userId: 1}
+        where: {
+            title: credentialData.title,
+            userId: user.id
+        }
     });
 
-    return result !== null;
+    return result !== null; 
+
 }
 
 const credentialRepository = {
-    insertCredential
+    insertCredential,
+    verifyTitle
 }
 
 export default credentialRepository;
--- src/services/credential-service.ts
+++ src/services/credential-service.ts
@@ -1,28 +1,16 @@
 import { CredentialData, UserPayload } from "../protocols";
-import { invalidError, unauthorizedError } from "../errors/error";
+import { conflictError, invalidError, unauthorizedError } from "../errors/error";
 import credentialRepository from "../repositories/credential-repository";
-import Cryptr from "cryptr";
-
-const cryptr = new Cryptr(process.env.CRYPTR_SECRET as string);
-
 
 async function insertCredential(credentialData: CredentialData, user: UserPayload) {
 
-    // const userId = await signinRepository.verifyUser()
+    const titleExist = await credentialRepository.verifyTitle(credentialData, user);
+
+    if (titleExist) {
+        throw conflictError("Título");
+    }
     
-    const result = await credentialRepository.insertCredential(credentialData, user);
-
-    // if (!emailExists) {
-    //     throw invalidError("E-mail");
-    // }
-
-    // const isPasswordValid = await signinRepository.verifyPassword(userData);
-
-    // if (!isPasswordValid) {
-    //     throw unauthorizedError("Senha");
-    // }
-    
-
+    const result = await credentialRepository.insertCredential(credentialData, user);  
     return result;
 }
 
